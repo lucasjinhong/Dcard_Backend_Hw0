7f3b14038d03373884c3d04d728a7cba8c2df678
lucasjinhong <55587534+lucasjinhong@users.noreply.github.com>
2022-03-29 19:16:46 +0800

    change folder

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 var createError = require('http-errors');
-var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
+
+var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const port = process.env.PORT || 8000;
+
+var urlRouter = require('./server_code/routes/url');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', urlRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,3 +41,6 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+app.listen(port);
+console.log('\nServer started at http://35.77.213.217:' + port );
---
+++ tests/url.test.js
@@ -0,0 +1,21 @@
+const request = require("supertest");
+const { response } = require("./app");
+const app = require("./app");
+
+describe('url API', () => {
+    it('GET /url_id --> show the original url', () =>{
+
+    });
+
+    it('GET /url_id --> 404 not found', () => {
+
+    });
+
+    it('POST /url --> created a new shorten_url', () => {
+
+    });
+
+    it('GET /url --> validates request body', () => {
+
+    });
+})
